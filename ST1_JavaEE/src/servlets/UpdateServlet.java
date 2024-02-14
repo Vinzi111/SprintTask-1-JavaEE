2cc7a082c99ed131d291fdfce15e4f421a33ddf6
Vinzi111 <106618578+Vinzi111@users.noreply.github.com>
2024-02-14 23:54:02 +0600

    Delete ST1_JavaEE directory

--- ST1_JavaEE/src/servlets/UpdateServlet.java
+++
@@ -1,9 +0,0 @@
-package servlets;
-
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-
-@WebServlet(value = "/updateTask")
-public class UpdateServlet extends HttpServlet {
-
-}
